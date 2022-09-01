8444180e959d7e2e1b4bb21b473c88dd98f70921
ase19410008 <65647081+ase19410008@users.noreply.github.com>
2022-09-01 10:43:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from ginza import *
 import streamlit as st
 from spacy_streamlit import visualize_parser, process_text
 
+st.title("ウミガメのスープの形態素解析")
+
+"ある男がバーに入ってきて、バーテンダーに水を一杯注文した。バーテンダーは銃を取り出し、男に狙いをつけて撃鉄を上げた。男は「ありがとう」と言って帰って行った。一体どういうことか？"
+
 nlp = spacy.load("ja_core_news_md")
 doc1 = nlp("ある男がバーに入ってきて、バーテンダーに水を一杯注文した。バーテンダーは銃を取り出し、男に狙いをつけて撃鉄を上げた。男は「ありがとう」と言って帰って行った。一体どういうことか？")
 visualize_parser(doc1)
